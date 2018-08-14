c7c1c98088096af2b303d7debbd9371265bc1d61
Joël LAMOTTE <jlamotte@softbankrobotics.com>
2018-08-14 15:51:37 +0200

    Initial project generated through `bdep new`

---
+++ mylib-versionned/mylib.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iosfwd>
+#include <string>
+
+#include <mylib-versionned/export.hpp>
+
+namespace mylib
+{
+  // Print a greeting for the specified name into the specified
+  // stream. Throw std::invalid_argument if the name is empty.
+  //
+  MYLIB_VERSIONNED_SYMEXPORT void
+  say_hello (std::ostream&, const std::string& name);
+}
